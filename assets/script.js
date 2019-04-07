eebc6b4449a732244c899fcbd6d4ba330149abfc
Ryan Gartner <ryanjgartner@gmail.com>
2019-04-07 11:16:49 -0500

    Updates

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,26 @@
 // Animated Background
 
-window.onload = function() {
-    Particles.init({
-      selector: '.background'
-    });
-  };
+window.onload = function () {
+  Particles.init({
+    selector: '.background'
+  });
+};
+
+// technologies//
+var cube = document.querySelector('.cube');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if (currentClass) {
+    cube.classList.remove(currentClass);
+  }
+  cube.classList.add(showClass);
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener('change', changeSide);
